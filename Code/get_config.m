4a68cfe41454823625cb9c7be9b5446448cbe9da
Dylan Winters <dylan@dw-mbp.local>
2017-08-23 11:42:58 -0700

    check for missing configuration options

--- Code/get_config.m
+++ Code/get_config.m
@@ -12,4 +12,24 @@ if exist(cruise_file) == 2
     feval(['config_' cruise])
 end
 
+% Check for missing options
+opts = {'freq_base';
+        'dir_raw';
+        'dir_deployment';
+        'dn_range';
+        'sensor_sn';
+        'sensor_pos'};
+msg = '';
+n_missing = 0;
+for i = 1:length(opts)
+    if ~isfield(config,opts{i})
+        msg = sprintf('%s\n  %s',msg,opts{i});
+        n_missing = n_missing + 1;
+    end
+end
+if n_missing > 0
+    error('Missing configuration for deployment "%s" in %s.m:%s',...
+          dep,cruise_file,msg);
+end
+
 config.sensor_sn = reshape(config.sensor_sn,1,[]);
