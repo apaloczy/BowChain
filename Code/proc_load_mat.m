3c769dd0c7437db96bfc31eae9b1f5d076d5d737
Dylan Winters <dylan@dw-mbp.local>
2017-08-23 13:11:54 -0700

    implement data loading

--- Code/proc_load_mat.m
+++ Code/proc_load_mat.m
@@ -1,11 +1,11 @@
 function data = proc_load_mat(config)
-data.sensors = containers.Map();
+data = containers.Map();
 
+disp('Loading raw data from .mat files...')
 for sn_cell = keys(config.sensors)
-    sn = char(sn_cell)
-    [~,fname,fext] = fileparts(config.sensors(sn).file_mat);
-    % disp(sprintf('Loaded %s',[fname fext]))
-    %* Fill data structure here
-    data.sensors(sn) = struct();
+    sn = char(sn_cell);
+    data(sn) = load(config.sensors(sn).file_mat);
+    msg = '  Loaded data from %s [%s]';
+    disp(sprintf(msg,config.sensors(sn).sensor_type,sn))
 end    
     
