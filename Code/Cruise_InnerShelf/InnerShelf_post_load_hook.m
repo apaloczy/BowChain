f746a0545cb3157c3f58c4822b696bf497b499c9
Dylan Winters <dylan.s.winters@gmail.com>
2018-02-13 18:40:35 -0800

    correct handling of GusT data structures

--- Code/Cruise_InnerShelf/InnerShelf_post_load_hook.m
+++ Code/Cruise_InnerShelf/InnerShelf_post_load_hook.m
@@ -1,12 +1,15 @@
 function [data,cfg] = InnerShelf_post_load_hook(data,cfg);
 
-for i = 1:length(data)
-    switch data{i}.sn
+% proc_load_mat.m loaded pre-existing GusT datafiles. Convert these to a format
+% consistent with output returned by the rest of the parsing functions.
+for i = 1:length(cfg.sensors)
+    switch cfg.sensors(i).sn
       case {'G049','G050'};
         d = struct();
-        d.dn = data(sns{i}).GusT.dnum;
-        d.t = data(sns{i}).GusT.t;
-        d.p = data(sns{i}).GusT.p;
+        d.dn = data{i}.GusT.dnum;
+        d.t = data{i}.GusT.t;
+        d.p = data{i}.GusT.p;
+        d.sn = cfg.sensors(i).sn;
         data{i} = d;
     end
 end
