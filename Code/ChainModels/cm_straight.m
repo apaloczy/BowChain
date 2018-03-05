6daf959be9ccf6ea88e374fdf445dda52b5e3d02
Dylan Winters <dylan.s.winters@gmail.com>
2018-03-05 13:50:06 -0800

    Don't extrapolate pressure with only 1 sensor

--- Code/ChainModels/cm_straight.m
+++ Code/ChainModels/cm_straight.m
@@ -3,8 +3,15 @@ function gridded = cm_straight(gridded,cfg)
 % Assume the chain is completely straight. Calculate vertical positions by
 % interpolating pressure vs. position.
 hasP = ~all(isnan(gridded.p),2);
-gridded.z = -interp1(gridded.pos(hasP),gridded.p(hasP,:),gridded.pos,'linear','extrap');
+if sum(hasP) > 1
+    gridded.z = -interp1(gridded.pos(hasP),gridded.p(hasP,:),gridded.pos,'linear','extrap');
+else    
+    gridded.z = nan(size(gridded.x));
+end
+
+% This model assumes no horizontal offsets
 gridded.x = zeros(size(gridded.x));
+
 
 % dl = diff(gridded.pos*ones(1,length(gridded.dn)));
 % dz = diff(gridded.z);
